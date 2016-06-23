70bd09813d1f7c2666d5cf30082e19ad02b8bcaa
toycom96 <toycom96@naver.com>
2016-06-23 20:15:57 +0900

    메뉴, 아이콘 편집

--- app/src/main/java/com/example/ethan/share01/MainActivity.java
+++ app/src/main/java/com/example/ethan/share01/MainActivity.java
@@ -115,13 +115,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.time_share) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.talent_share) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.goods_share) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.setting) {
 
         } else if (id == R.id.nav_share) {
 

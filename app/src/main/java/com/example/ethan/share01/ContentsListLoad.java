8598c8a22fcb00ea7feceeefe482f006f24e5a23
toycom96 <toycom96@naver.com>
2016-06-28 02:25:23 +0900

    list up function alpha version finish.

--- app/src/main/java/com/example/ethan/share01/ContentsListLoad.java
+++ app/src/main/java/com/example/ethan/share01/ContentsListLoad.java
@@ -1,5 +1,19 @@
 package com.example.ethan.share01;
 
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -7,31 +21,105 @@ import java.util.List;
  */
 public class ContentsListLoad {
 
-    private List<ContentsListObject> mContentItem;
-    public ContentsListAdapter mAdapter;
+    static List<ContentsListObject> mContentItem;
+    static ContentsListAdapter mAdapter;
+
 
     public ContentsListLoad (List<ContentsListObject> ContentItem, ContentsListAdapter ListAdapter) {
         this.mContentItem = ContentItem;
         this.mAdapter = ListAdapter;
     }
 
-    public int LoadFromApi(int ListIndex, int flag) {
-        int id = ListIndex + 1;
-
-        this.mContentItem.add(new ContentsListObject(id, "https://i.ytimg.com/vi/OMB4TS6sCYo/hqdefault.jpg", "George Orwell"));
-        this.mContentItem.add(new ContentsListObject(id+1, "http://i.kinja-img.com/gawker-media/image/upload/dqhwzcryunyv6tznrqjy.jpg", "George Orwell"));
-        this.mContentItem.add(new ContentsListObject(id+2, "http://cars.mclaren.com/files/live/sites/mclaren/files/cars-mclaren-com-Main/McLaren%20Model%20Section/650S%20Coupe/Design/650S_Hero_Design_Height4.jpg?t=w1440", "George Orwell"));
-        this.mContentItem.add(new ContentsListObject(id+3, "http://cars.mclaren.com/files/live/sites/mclaren/files/cars-mclaren-com-Main/McLaren%20Model%20Section/GT%20Sprint%20650S/McLaren_650SGTSprint_front3q_2f-Edit.jpg", "George Orwell"));
-        this.mContentItem.add(new ContentsListObject(id+4, "http://images.cdn.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/mclaren-650s-rt-325_0.jpg", "George Orwell"));
-        this.mContentItem.add(new ContentsListObject(id+5, "http://www.telegraph.co.uk/cars/images/2015/12/30/McLaren-650S-front-large_trans++_emuMSIF920-7wOLsuHQL5-4UITJmy5HzCeWHAP_eZw.jpg", "George Orwell"));
-        this.mContentItem.add(new ContentsListObject(id+6, "http://ag-spots-2014.o.auroraobjects.eu/2014/06/23/mclaren-650s-c489723062014153820_2.jpg", "George Orwell"));
-        this.mContentItem.add(new ContentsListObject(id+7, "http://cdn9.themanual.com/wp-content/uploads/2014/04/2015-McLaren-650S-22.jpg", "George Orwell"));
-        this.mContentItem.add(new ContentsListObject(id+8, "http://pullzone1.motoringresearch.netdna-cdn.com/wp-content/uploads/2014/03/McLaren-650S-orange.jpg", "George Orwell"));
-        this.mContentItem.add(new ContentsListObject(id+9, "http://bitcast-r.v1.sjc1.bitgravity.com/wapitltd/images2/550000/9000/900/559912.jpg", "George Orwell"));
-
-        int curSize = mAdapter.getItemCount();
-        mAdapter.notifyItemRangeInserted(curSize, mContentItem.size() - 1);
+    public int loadFromApi(int ListIndex, int flag) {
+        getBbsList BbsList = new getBbsList(mContentItem, mAdapter);
+        BbsList.execute("0", "1");
 
         return 0;
+    }
+
+    static int updateUi() {
+        int curSize = mAdapter.getItemCount();
+        mAdapter.notifyItemRangeInserted(curSize, mContentItem.size() - 1);
+        return 0;
+    }
+
+
+    class getBbsList extends AsyncTask<String, Void, Void> {
+        private List<ContentsListObject> mContentItem;
+        public ContentsListAdapter mAdapter;
+
+
+        public getBbsList (List<ContentsListObject> ContentItem, ContentsListAdapter ListAdapter) {
+            this.mContentItem = ContentItem;
+            this.mAdapter = ListAdapter;
+        }
+
+        protected void onPostExecute(Void aVoid) {
+            ContentsListLoad.updateUi();
+        }
+
+        @Override
+        protected Void doInBackground(String... value) {
+            HttpURLConnection conn = null;
+            OutputStream os = null;
+            InputStream is = null;
+            ByteArrayOutputStream baos = null;
+            String response = null;
+
+            try {
+                IgnoreHttpSertification.ignoreSertificationHttps();
+                URL obj = new URL("https://toycom96.iptime.org:1443/bbs_list");
+                conn = (HttpURLConnection) obj.openConnection();
+                conn.setConnectTimeout(3000);
+                conn.setReadTimeout(5000);
+                conn.setRequestMethod("POST");
+
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setRequestProperty("Cookie", "auth=NtUMVRdHefRNbYut82ALIz0kE6a6Q80D13krg/xdxWfi8E3H+nqv5+54Xxo49D4n8tNaTQ==");
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
+
+
+                JSONObject job = new JSONObject();
+                job.put("long", 126.7459979);
+                job.put("lat", 37.259485);
+                job.put("dist", 10000);
+                job.put("lidx", 0);
+
+                os = conn.getOutputStream();
+                os.write(job.toString().getBytes("utf-8"));
+                os.flush();
+
+                int responseCode = conn.getResponseCode();
+                //request code를 받음
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    is = conn.getInputStream();
+                    baos = new ByteArrayOutputStream();
+                    byte[] byteBuffer = new byte[1024];
+                    byte[] byteData = null;
+                    int nLength = 0;
+                    while ((nLength = is.read(byteBuffer, 0, byteBuffer.length)) != -1) {
+                        baos.write(byteBuffer, 0, nLength);
+                    }
+                    byteData = baos.toByteArray();
+
+                    response = new String(byteData);
+                }
+
+                String result = "";
+                JSONArray ja = new JSONArray(response);
+                for (int i = 0; i < ja.length(); i++) {
+                    JSONObject order = ja.getJSONObject(i);
+
+                    this.mContentItem.add(new ContentsListObject(order.getInt("Id"), order.getString("Media"), order.getString("Title")));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }

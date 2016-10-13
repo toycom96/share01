ca09a4885e3206317bdd3d7083e76fe1a7047098
leedongkwang <toycom96@naver.com>
2016-10-13 22:17:41 +0900

    게시물 상세페이지 작업중. 댓글부분 작업중

--- app/src/main/java/com/example/ethan/share01/BbsDetailActivity.java
+++ app/src/main/java/com/example/ethan/share01/BbsDetailActivity.java
@@ -10,10 +10,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
@@ -21,6 +23,10 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+
+import com.example.ethan.share01.model.BbsMemo;
+import com.example.ethan.share01.adapter.BbsMemoAdapter;
 
 public class BbsDetailActivity extends AppCompatActivity {
 
@@ -29,7 +35,6 @@ public class BbsDetailActivity extends AppCompatActivity {
     private TextView bbs_opt;
     private TextView bbs_etc;
     private ImageView bbs_photo;
-
 
     private String getBbs_title;
     private String getBbs_msg;
@@ -47,7 +52,25 @@ public class BbsDetailActivity extends AppCompatActivity {
     private double mLon;
     private int photo_idx = 0;
 
+
+    ////////////////////////////////////////////////////////
+    private ListView BbsMemo_lv;
+    private ArrayList<BbsMemo> mBbsMemo = null;
+    private BbsMemoAdapter mBbsMemoAdapter;
+
+    //int bbs_id, int memo_id, int user_id, String user_name, int user_age, String user_sex, String user_photo, String memo, String date)
+    private int getBbs_id;
+    private int getBbsMemo_userid;
+    private String getBbsMemo_username;
+    private int getBbsMemo_userage;
+    private String getBbsMemo_usersex;
+    private String getBbsMemo_userphoto;
+    private String getBbsMemo_memo;
+    private String getBbsMemo_date;
+    private String getBbsMemo_term;
+
     private final String bbs_detail_url = "https://toycom96.iptime.org:1443/bbs_view";
+    private final String bbsmemo_detail_url = "https://toycom96.iptime.org:1443/memo_list";
     public static RbPreference mPref;
     public GpsInfo mGps;
 
@@ -115,6 +138,10 @@ public class BbsDetailActivity extends AppCompatActivity {
 
         GetBbsDetailThread info = new GetBbsDetailThread();
         info.execute(bbs_detail_url, mPref.getValue("auth", ""));
+
+        BbsMemoLoadThread BbsMemo = new BbsMemoLoadThread();
+        BbsMemo.execute(bbsmemo_detail_url, mPref.getValue("auth", ""));
+
     }
 
     class GetBbsDetailThread extends AsyncTask<String, Void, Void> {
@@ -265,6 +292,150 @@ public class BbsDetailActivity extends AppCompatActivity {
                     getBbs_photo_url[3] = mediaJson.getString("img3");
                     getBbs_photo_url[4] = mediaJson.getString("img4");
                     //getBbs_photo_url = responseJSON.get("Media").toString();
+                }else {
+                    Log.e("HTTP_ERROR", "NOT CONNECTED HTTP");
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+
+    class BbsMemoLoadThread extends AsyncTask<String, Void, Void> {
+
+        ProgressDialog loading;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            loading = new ProgressDialog(BbsDetailActivity.this);
+            loading.setTitle("댓글 리스트");
+            loading.setMessage("댓글 리스트를 받는 중이에요...");
+            loading.setCancelable(false);
+            loading.show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            mBbsMemoAdapter = new BbsMemoAdapter(BbsDetailActivity.this, mBbsMemo);
+
+            BbsMemo_lv.setAdapter(mBbsMemoAdapter);
+            loading.dismiss();
+        }
+
+        @Override
+        protected Void doInBackground(String... value) {
+            HttpURLConnection conn = null;
+            OutputStream os = null;
+            InputStream is = null;
+            ByteArrayOutputStream baos = null;
+            String response = null;
+            /*
+            http통신 부분 설정 변수들
+             */
+            String connUrl = value[0];
+            String user_auth = value[1];
+
+            try {
+                IgnoreHttpSertification.ignoreSertificationHttps();
+                //String url = "https://toycom96.iptime.org:1443/user_join";
+                URL obj = new URL(connUrl);
+                //접속 Server URL 설정
+                conn = (HttpURLConnection) obj.openConnection();
+                //Http 접속
+                conn.setConnectTimeout(10000);
+                //접속 timeuot시간 설정
+                conn.setReadTimeout(10000);
+                //read timeout 시간 설정
+                conn.setRequestMethod("POST");
+                //통신 방식 : POST
+
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty("Accept", "application/json");
+                //데이터 주고 받는 형식 : json 설정
+                conn.addRequestProperty("Cookie", user_auth);
+                //Cookie값 설정(auth)
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
+
+
+                os = conn.getOutputStream();
+                //Output Stream 생성
+                os.flush();
+                //Buffer에 있는 모든 정보를 보냄
+
+                int responseCode = conn.getResponseCode();
+
+                //int responseCode = conn.getResponseCode();
+                //request code를 받음
+
+                if(responseCode == HttpURLConnection.HTTP_OK) {
+                    int unix_sec = 0;
+
+                    Log.e("HTTP_OK", "HTTP OK RESULT");
+                    is = conn.getInputStream();
+                    baos = new ByteArrayOutputStream();
+                    byte[] byteBuffer = new byte[1024];
+                    byte[] byteData = null;
+                    int nLength = 0;
+                    while((nLength = is.read(byteBuffer, 0, byteBuffer.length)) != -1) {
+                        baos.write(byteBuffer, 0, nLength);
+                    }
+                    byteData = baos.toByteArray();
+
+                    response = new String(byteData);
+                    //Json 문자열로 온 데이터값을 저장함( ex.> {"key":value} )
+
+
+                    if (response.isEmpty() || response.equals("null") ) {
+                        Log.e("ChatListNull", response.toString());
+                        return null;
+                    }
+                    mBbsMemo.clear();
+
+                    JSONArray ja = new JSONArray(response);
+                    for (int i = 0; i < ja.length(); i++) {
+                        JSONObject order = ja.getJSONObject(i);
+
+                        getBbs_id = Integer.parseInt(order.get("Bbs_id").toString());
+                        getBbsMemo_userid = Integer.parseInt(order.get("User_id").toString());
+                        getBbsMemo_userage = Integer.parseInt(order.get("User_age").toString());
+                        getBbsMemo_username = order.get("User_name").toString();
+                        getBbsMemo_usersex = order.get("User_sex").toString();
+                        getBbsMemo_userphoto = order.get("User_photo").toString();
+                        getBbsMemo_memo = order.get("Msg").toString();
+                        getBbsMemo_date = order.get("Created").toString();
+                        getBbsMemo_term = order.get("Created").toString();
+                        //unix_sec = Integer.parseInt(order.get("Sended").toString());
+
+
+                        /*if ( unix_sec > (6 * 30 * 24 * 60 * 60) ) { getTime = "반년이상"; }
+                        else if ( unix_sec > ( 30 * 24 * 60 * 60) ) { getTime = (unix_sec / ( 30 * 24 * 60 * 60)) + "개월전"; }
+                        else if ( unix_sec > ( 24 * 60 * 60) ) { getTime = (unix_sec / ( 24 * 60 * 60)) + "일전"; }
+                        else if ( unix_sec > ( 60 * 60) ) { getTime = (unix_sec / ( 60 * 60)) + "시간전"; }
+                        else if ( unix_sec > ( 60) ) { getTime = (unix_sec / (60)) + "분전"; }
+                        else { getTime = unix_sec + "초전"; }
+                        //getTime = order.get("Sended").toString();
+
+
+                        if (getBbsMemo_usersex == "F") {
+                            getEtcInfo = "여 " + order.get("User_age").toString() + "세";
+                        } else {
+                            getEtcInfo = "남 " + order.get("User_age").toString() + "세";
+                        }*/
+                        mBbsMemo.add(new BbsMemo(getBbs_id, 0, getBbsMemo_userid, getBbsMemo_username, getBbsMemo_userage,  getBbsMemo_usersex, getBbsMemo_userphoto, getBbsMemo_memo, getBbsMemo_date));
+                    }
+                    Log.i("Response Data", response);
+                    //JSONObject responseJSON = new JSONObject(response);
+                    //JSONObject를 생성해 key값 설정으로 result값을 받음.
+
+
+
                 }else {
                     Log.e("HTTP_ERROR", "NOT CONNECTED HTTP");
                 }
---
+++ app/src/main/java/com/example/ethan/share01/adapter/BbsMemoAdapter.java
@@ -0,0 +1,79 @@
+package com.example.ethan.share01.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.ethan.share01.R;
+import com.example.ethan.share01.model.BbsMemo;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by leedongkwang on 2016. 10. 12..
+ */
+
+
+public class BbsMemoAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private ArrayList<BbsMemo> mBbsMemoList;
+
+    public BbsMemoAdapter(Context context, ArrayList<BbsMemo> bbsMemoList) {
+        mContext = context;
+        mBbsMemoList = bbsMemoList;
+    }
+
+    @Override
+    public int getCount() {
+        return mBbsMemoList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mBbsMemoList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = View.inflate(mContext, R.layout.custom_chat_listview, null);
+        TextView title = (TextView) v.findViewById(R.id.chatlist_title);
+        TextView msg = (TextView) v.findViewById(R.id.chatlist_msg);
+        TextView time = (TextView) v.findViewById(R.id.chatlist_time);
+        TextView etc = (TextView) v.findViewById(R.id.chatlist_etc); //거리
+        //TextView badge = (TextView) v.findViewById(R.id.chatlist_msgcnt);
+
+        BbsMemo item = mBbsMemoList.get(position);
+
+        title.setText(item.getUser_name());
+        if (item.getUser_sex().equals("F")) { title.setTextColor(Color.parseColor("#FF0000")); }
+        else { title.setTextColor(Color.parseColor("#0000FF")); }
+        msg.setText(item.getMemo());
+        time.setText(item.getTerm());
+        etc.setText(item.getUser_sex() + " / " + item.getUser_age() +"세");
+        if (!item.getUser_photo().toString().isEmpty()) {
+            CircleImageView photo = (CircleImageView) v.findViewById(R.id.chatlist_photo);
+            Picasso.with(mContext).load(item.getUser_photo().toString()).resize(72, 72).into(photo);
+        }
+        /*if (item.getBadgeCnt() > 0) {
+            badge.setBackgroundResource(R.drawable.ic_badge_new);
+
+            //} else {
+            //    badge.setBackground("");
+        }*/
+
+        return v;
+    }
+}
